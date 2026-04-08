9adca8796ffcd1bafa5973dbbe3fa21426c0e95d
Noyan <noyannowhasyt@gmail.com>
2026-04-08 12:07:31 +0300

    Update Website.js

--- js/components/Sidebar/Website.js
+++ js/components/Sidebar/Website.js
@@ -6,6 +6,9 @@ export default {
                 <a href="https://noyandemons.ps.fhgdps.com/dashboard" target="_blank" rel="noopener">
                     Go to Dashboard
                 </a>
+                <a href="https://slowly-planned-signed-divisions.trycloudflare.com/demonlist/" target=_blank" rel="noopener">
+                    New New Demon List
+                </a>
             </p>
         </div>
     `
