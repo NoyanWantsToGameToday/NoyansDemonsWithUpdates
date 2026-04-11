79e3135941bcf7a2504f8fd3ba0354890f51f9fa
Noyan <noyannowhasyt@gmail.com>
2026-04-11 23:07:08 +0300

    Update Website.js

--- js/components/Sidebar/Website.js
+++ js/components/Sidebar/Website.js
@@ -11,7 +11,7 @@ export default {
         <h3>Newer Demon List</h3>
         <div class="right-text">
             <p>
-                <a href="https://advisor-intake-belt-work.trycloudflare.com" target=_blank" rel="noopener">
+                <a href="https://treasures-hrs-dual-pst.trycloudflare.com" target=_blank" rel="noopener">
                     New New Demon List
                 </a>
             </p>
