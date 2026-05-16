089b155a9e6677f7d443e8a11d26f303c99faa16
Noyan <noyannowhasyt@gmail.com>
2026-05-16 13:46:32 +0300

    Update Website.js

--- js/components/Sidebar/Website.js
+++ js/components/Sidebar/Website.js
@@ -11,7 +11,7 @@ export default {
         <h3>Newer Demon List</h3>
         <div class="right-text">
             <p>
-                <a href="https://speed-cleared-cook-tuner.trycloudflare.com" rel="noopener">
+                <a href="https://stuffed-blvd-cam-arising.trycloudflare.com" rel="noopener">
                     New New Demon List
                 </a>
             </p>
