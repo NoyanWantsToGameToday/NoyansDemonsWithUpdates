8ac631191a48f455787a3a5e97fa85eb6f333a24
Noyan <noyannowhasyt@gmail.com>
2026-06-04 10:06:56 +0300

    Update Website.js

--- js/components/Sidebar/Website.js
+++ js/components/Sidebar/Website.js
@@ -11,7 +11,7 @@ export default {
         <h3>Newer Demon List</h3>
         <div class="right-text">
             <p>
-                <a href="https://abstracts-ins-thumbnails-rank.trycloudflare.com" rel="noopener">
+                <a href="https://interact-forty-george-cover.trycloudflare.com" rel="noopener">
                     New New Demon List
                 </a>
             </p>
