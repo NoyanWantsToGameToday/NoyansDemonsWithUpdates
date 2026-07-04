0921e05fc9fd00096c45c879c09a2c642a73ae52
Noyan <noyannowhasyt@gmail.com>
2026-07-04 15:00:03 +0300

    Update Website.js

--- js/components/Sidebar/Website.js
+++ js/components/Sidebar/Website.js
@@ -11,7 +11,7 @@ export default {
         <h3>Newer Demon List</h3>
         <div class="right-text">
             <p>
-                <a href="https://vacation-illustrated-prophet-net.trycloudflare.com" rel="noopener">
+                <a href="https://dept-total-excited-bye.trycloudflare.com" rel="noopener">
                     New New Demon List
                 </a>
             </p>
