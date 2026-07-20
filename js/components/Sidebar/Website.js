412b4216b71a80ad4385e9c548b5e2b0e8cf191a
Noyan <noyannowhasyt@gmail.com>
2026-07-20 21:16:07 +0300

    Update Website.js

--- js/components/Sidebar/Website.js
+++ js/components/Sidebar/Website.js
@@ -11,7 +11,7 @@ export default {
         <h3>Newer Demon List</h3>
         <div class="right-text">
             <p>
-                <a href="https://temp-suburban-barrier-agreed.trycloudflare.com" rel="noopener">
+                <a href="https://consider-electric-object-terminal.trycloudflare.com" rel="noopener">
                     New Demon List
                 </a>
             </p>
